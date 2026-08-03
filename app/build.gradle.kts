5e434070ce80a13932d995a0f451b22aca71b2d5
ericFlash <34124384+ericFlash@users.noreply.github.com>
2026-08-03 14:34:30 +0800

    Add activity-ktx dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.activity:activity-ktx:1.9.3")
 
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.8.4")
